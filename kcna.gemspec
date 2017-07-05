d940f17c99d739fcfc030f09d14c30a427e1e6b6
hinamiyagk <syobon.hinata.public@gmail.com>
2017-07-05 23:52:09 +0900

    Implement set_language on KCNA

--- kcna.gemspec
+++ kcna.gemspec
@@ -15,6 +15,8 @@ Gem::Specification.new do |spec|
   spec.files = Dir["LICENSE", "README.md", "lib/**/*"]
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "httpclient", "~> 2.8"
+
   spec.add_development_dependency "bundler", "~> 1.15"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "minitest", "~> 5.0"
--- lib/kcna.rb
+++ lib/kcna.rb
@@ -1,5 +1,23 @@
 require "kcna/version"
+require "httpclient"
 
-module KCNA
-  # Your code goes here...
+class KCNA
+  KO = "kor"
+  EN = "eng"
+  ZH = "chn"
+  RU = "rus"
+  ES = "spn"
+  JA = "jpn"
+
+  def initialize
+    @client = HTTPClient.new
+  end
+
+  def set_language(lang)
+    data = {
+      article_code: "", article_type_list: "", news_type_code: "", show_what: "", mediaCode: "",
+      lang: lang
+    }
+    @client.post("http://kcna.kp/kcna.user.home.retrieveHomeInfoList.kcmsf", body: data)
+  end
 end
