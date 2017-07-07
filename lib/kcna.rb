8902e8a231171995bd6231d81d8db6d70fb24cb0
hinamiyagk <syobon.hinata.public@gmail.com>
2017-07-08 00:31:48 +0900

    Add KCNA#get_article method

--- lib/kcna.rb
+++ lib/kcna.rb
@@ -1,5 +1,8 @@
 require "kcna/version"
+require "kcna/article"
 require "httpclient"
+require "date"
+require "rexml/document"
 
 class KCNA
   KO = "kor"
@@ -13,11 +16,55 @@ class KCNA
     @client = HTTPClient.new
   end
 
+  def normalize_text(content)
+    great_leader_pattern = /<nobr><strong><font.*>(.*)<\/font><\/strong><\/nobr>/
+    patterns = ["\n", "<br>", "&nbsp;", great_leader_pattern]
+    content.gsub(Regexp.union(patterns)) do |match|
+      case match
+      when "\n", "&nbsp;"
+        ""
+      when "<br>"
+        "\n"
+      when great_leader_pattern
+        $1
+      end
+    end.sub(/(－－－|‐‐‐)$/, "")
+  end
+
   def set_language(lang)
     data = {
       article_code: "", article_type_list: "", news_type_code: "", show_what: "", mediaCode: "",
       lang: lang
     }
     @client.post("http://kcna.kp/kcna.user.home.retrieveHomeInfoList.kcmsf", body: data)
+  end
+
+  private def fetch_article(article_id)
+    article_url = "http://kcna.kp/kcna.user.article.retrieveArticleInfoFromArticleCode.kcmsf"
+    data = { article_code: "AR0099775", kwContent: "" }
+    @client.post(article_url, body: data, follow_redirect: true).body
+  end
+
+  def get_article(article_id)
+    doc = REXML::Document.new(fetch_article(article_id))
+    container = REXML::XPath.first(doc, "//NData")
+    raise "Article not found" if container.elements.size == 0
+
+    date = Date.strptime(REXML::XPath.first(doc, "//articleCreateDate").text, "%Y.%m.%d")
+    content = normalize_text(REXML::XPath.first(doc, "//content").text)
+    display_title = normalize_text(REXML::XPath.first(doc, "//dispTitle").text)
+    main_title = normalize_text(REXML::XPath.first(doc, "//mainTitle").text)
+    sub_title = normalize_text(REXML::XPath.first(doc, "//subTitle").text)
+    article_id = REXML::XPath.first(doc, "//articleCode").text
+    movie_count = REXML::XPath.first(doc, "//fMovieCnt").text.to_i
+    photo_count = REXML::XPath.first(doc, "//fPhotoCnt").text.to_i
+    music_count = REXML::XPath.first(doc, "//fMusicCnt").text.to_i
+
+    Article.new(
+      id: article_id, content: content,
+      date: date,
+      main_title: main_title, sub_title: sub_title, display_title: display_title,
+      movie_count: movie_count, photo_count: photo_count, music_count: music_count
+    )
   end
 end
---
+++ lib/kcna/article.rb
@@ -0,0 +1,21 @@
+class KCNA::Article
+  attr_reader :id
+
+  def self.attr_reader_hash(key, default = nil)
+    define_method(key, -> { @attrs[key].nil? ? default : @attrs[key] })
+  end
+
+  attr_reader_hash :date
+  attr_reader_hash :content
+  attr_reader_hash :main_title
+  attr_reader_hash :sub_title, ""
+  attr_reader_hash :display_title
+  attr_reader_hash :movie_count, 0
+  attr_reader_hash :photo_count, 0
+  attr_reader_hash :music_count, 0
+
+  def initialize(id:, **attrs)
+    @id = id
+    @attrs = attrs
+  end
+end
