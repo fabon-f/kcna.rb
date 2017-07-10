06abfca15f8c04182a58444d87d0f399de4802ac
hinamiyagk <syobon.hinata.public@gmail.com>
2017-07-10 16:03:08 +0900

    Make test code work on Ruby 2.2

--- test/kcna_test.rb
+++ test/kcna_test.rb
@@ -39,11 +39,11 @@ class KCNATest < Minitest::Test
   def test_get_article
     article =  kcna.get_article("AR0099775")
     assert_equal "AR0099775", article.id
-    assert_equal <<~EXPECTED.chomp, article.content
-    【平壌７月５日発朝鮮中央通信】金日成主席回顧インドネシア委員会が、６月２２日に結成された。
-    同回顧委員会の委員長にインドネシア先鋒者党中央指導理事会書記長のリスティヤント氏が選出された。
-    回顧委員会は、６月２５日から７月１０日までを回顧期間に定め、この期間に金日成主席の革命的生涯と業績をたたえる政治・文化行事を催すことにした。
-    EXPECTED
+    assert_equal <<EXPECTED.chomp, article.content
+【平壌７月５日発朝鮮中央通信】金日成主席回顧インドネシア委員会が、６月２２日に結成された。
+同回顧委員会の委員長にインドネシア先鋒者党中央指導理事会書記長のリスティヤント氏が選出された。
+回顧委員会は、６月２５日から７月１０日までを回顧期間に定め、この期間に金日成主席の革命的生涯と業績をたたえる政治・文化行事を催すことにした。
+EXPECTED
     assert_equal "金日成主席回顧インドネシア委を結成", article.main_title
     assert Date.parse("2017-07-05") === article.date, "Expected: 2017-07-05, Actual: #{article.date}"
     assert_equal 0, article.photo_count
